9c7b75f6d708cfb45e32cf97cfb5ec3471603ae3
Juan Felipe Ramirez <juanfeliperamirezsanchez@gmail.com>
2021-07-14 00:26:21 -0500

    Pagina de los departamentos finalizadas, tanto html, como css y js, queda finalizada

---
+++ DepartamentosJuanRamirez/departamentos.js
@@ -0,0 +1,160 @@
+let amazonas = document.getElementById("amazonas");
+let antioquia = document.getElementById("antioquia");
+let arauca = document.getElementById("arauca");
+let bolivar = document.getElementById("bolivar");
+let boyaca = document.getElementById("boyaca");
+let caldas = document.getElementById("caldas");
+let caqueta = document.getElementById("caqueta");
+let casanare = document.getElementById("casanare");
+let cauca = document.getElementById("cauca");
+let cesar = document.getElementById("cesar");
+let choco = document.getElementById("choco");
+let cordoba = document.getElementById("cordoba");
+let cundinamarca = document.getElementById("cundinamarca");
+let guainia = document.getElementById("guainia");
+let guaviare = document.getElementById("guaviare");
+let huila = document.getElementById("huila");
+let guajira = document.getElementById("guajira");
+let magdalena = document.getElementById("magdalena");
+let meta = document.getElementById("meta");
+let narino = document.getElementById("narino");
+let nsantander = document.getElementById("nsantander");
+let putumayo = document.getElementById("putumayo");
+let quindio = document.getElementById("quindio");
+let risaralda = document.getElementById("risaralda");
+let andres = document.getElementById("andres");
+let santander = document.getElementById("santander");
+let sucre = document.getElementById("sucre");
+let tolima = document.getElementById("tolima");
+let vcauca = document.getElementById("vcauca");
+let vaupes = document.getElementById("vaupes");
+let vichada = document.getElementById("vichada");
+
+amazonas.onclick = function() {
+    return confirm("La capital de amazonas es Leticia, quieres saber mas de esta ciudad?")
+};
+
+antioquia.onclick = function() {
+    return confirm("La capital de Antioquia es Medellin, quieres saber mas de esta ciudad?")
+};
+
+arauca.onclick = function() {
+    return confirm("La capital de Arauca es Arauca, quieres saber mas de esta ciudad?")
+};
+
+atlantico.onclick = function() {
+    return confirm("La capital de Atlantico es Barranquilla, quieres saber mas de esta ciudad?")
+};
+
+bolivar.onclick = function() {
+    return confirm("La capital de Bolivar es Cartagena, quieres saber mas de esta ciudad?")
+
+};
+
+boyaca.onclick = function() {
+    return confirm("La capital de Boyaca es Tunja, quieres saber mas de esta ciudad?")
+};
+
+caldas.onclick = function() {
+    return confirm("La capital de Caldas es Manizales, quieres saber mas de esta ciudad?")
+};
+
+caqueta.onclick = function() {
+    return confirm("La capital de Caqueta es Florencia, quieres saber mas de esta ciudad?")
+};
+
+casanare.onclick = function() {
+    return confirm("La capital de Casanare es Yopal, quieres saber mas de esta ciudad?")
+};
+
+cauca.onclick = function() {
+    return confirm("La capital de Cauca es Popayán, quieres saber mas de esta ciudad?")
+};
+
+cesar.onclick = function() {
+    return confirm("La capital de Cesar es Valledupar, quieres saber mas de esta ciudad?")
+};
+
+choco.onclick = function() {
+    return confirm("La capital de Choco es Quibdo, quieres saber mas de esta ciudad?")
+};
+
+cordoba.onclick = function() {
+    return confirm("La capital de Cordoba es Monteria, quieres saber mas de esta ciudad?")
+};
+
+cundinamarca.onclick = function() {
+    return confirm("La capital de Cundinamarca es Bogota, quieres saber mas de esta ciudad?")
+};
+
+guainia.onclick = function() {
+    return confirm("La capital de Guainia es Inirida, quieres saber mas de esta ciudad?")
+};
+
+guaviare.onclick = function() {
+    return confirm("La capital de Guaviare es San Jose Del Guaviare, quieres saber mas de esta ciudad?")
+};
+
+huila.onclick = function() {
+    return confirm("La capital de Huila es Neiva, quieres saber mas de esta ciudad?")
+};
+
+guajira.onclick = function() {
+    return confirm("La capital de Guajira es Riohacha, quieres saber mas de esta ciudad?")
+};
+
+magdalena.onclick = function() {
+    return confirm("La capital de Magdalena es Santa Marta, quieres saber mas de esta ciudad?")
+};
+
+meta.onclick = function() {
+    return confirm("La capital de Meta es Villavicencio, quieres saber mas de esta ciudad?")
+};
+
+narino.onclick = function() {
+    return confirm("La capital de Narino es Pasto, quieres saber mas de esta ciudad?")
+};
+
+nsantander.onclick = function() {
+    return confirm("La capital de Norte de Santander es Cucuta, quieres saber mas de esta ciudad?")
+};
+
+putumayo.onclick = function() {
+    return confirm("La capital de Putumayo es Mocoa, quieres saber mas de esta ciudad?")
+};
+
+quindio.onclick = function() {
+    return confirm("La capital de Quindio es Armenia, quieres saber mas de esta ciudad?")
+};
+
+risaralda.onclick = function() {
+    return confirm("La capital de Risaralda es Pereira, quieres saber mas de esta ciudad?")
+};
+
+andres.onclick = function() {
+    return confirm("La capital de San Andres y Providencia es San Andres, quieres saber mas de esta ciudad?")
+};
+
+santander.onclick = function() {
+    return confirm("La capital de Santander es Bucaramanga, quieres saber mas de esta ciudad?")
+};
+
+sucre.onclick = function() {
+    return confirm("La capital de Sucre es Sincelejo, quieres saber mas de esta ciudad?")
+};
+
+tolima.onclick = function() {
+    return confirm("La capital de Tolima es Ibague, quieres saber mas de esta ciudad?")
+};
+
+vcauca.onclick = function() {
+    return confirm("La capital de Valle del Cauca es Cali, quieres saber mas de esta ciudad?")
+};
+
+vaupes.onclick = function() {
+    return confirm("La capital de Vaupes es Mitu, quieres saber mas de esta ciudad?")
+};
+
+vichada.onclick = function() {
+    return confirm("La capital de Vichada es Puerto Carreño, quieres saber mas de esta ciudad?")
+};
